9e0184dbc3ad0004746b5757b377db617c4a31f2
HEINRICK7 <heinrickcostta@gmail.com>
2024-08-24 13:50:24 -0300

    update projects

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 // src/App.js
-import React from 'react';
-import './App.css';
-import MapComponent from '../src/components/MapComponent.js';
+import React from "react";
+import "./App.css";
+import MapComponent from "../src/components/MapComponent.js";
 
 function App() {
-    return (
-        <div className="App">
-            <h1>Mapa de Piripiri - PI</h1>
-            <MapComponent />
-        </div>
-    );
+  return (
+    <div className="App">
+      <header
+        style={{
+          textAlign: "center",
+        }}
+      >
+        <h1>Intenção de Votos</h1>
+        <p>
+          Esta aplicação permite que os eleitores registrem sua intenção de voto
+          nos candidatos locais.
+        </p>
+        <p>
+          Utilize o mapa abaixo para selecionar sua região e escolher seu
+          candidato.
+        </p>
+      </header>
+      <MapComponent />
+    </div>
+  );
 }
 
 export default App;
